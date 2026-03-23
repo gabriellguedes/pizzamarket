e5caf3ec92513d044bc1680efcaf0f080fb7ce1e
gabriellguedes <gguedes10@gmail.com>
2026-03-23 19:25:28 -0300

    Instalação do Django Cors Headers.

--- engine/settings.py
+++ engine/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'engine.core',
     #REST Framework
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'engine.urls'
@@ -122,3 +124,5 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+CORS_ALLOW_ALL_ORIGINS = True
