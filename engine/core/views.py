34082db720e0c6bfb44df9fafd4ee80853037858
gabriellguedes <gguedes10@gmail.com>
2026-03-23 19:22:10 -0300

    Criando a View do Produto.

--- engine/core/views.py
+++ engine/core/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import Pedido
-from .serializers import PedidoSerializer
+from .models import Pedido, Produto
+from .serializers import PedidoSerializer, ProdutoSerializer
 
 # Create your views here.
 class PedidoViewSet(viewsets.ModelViewSet):
     queryset = Pedido.objects.all()
     serializer_class = PedidoSerializer
+
+class ProdutoViewSet(viewsets.ModelViewSet):
+    queryset = Produto.objects.all()
+    serializer_class = ProdutoSerializer
